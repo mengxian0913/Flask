48ce68d277b23d02947f120cb78182c07791e469
mengxain0913 <mengxian0913@gmail.com>
2023-01-23 11:53:10 +0800

    new

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup as bs
 
-url = ""
+url = "https://pic.sogou.com/pic/searchList.jsp?statref=searchlist_hintword_up&keyword=%E8%9C%A1%E7%AC%94%E5%B0%8F%E6%96%B0%E5%9B%BE%E7%89%87%E5%A4%B4%E5%83%8F%E5%8F%AF%E7%88%B1&spver=0"
 response = requests.get(url)
 soup = bs(response.content, "html.parser")
 img_tags = soup.find_all("img")
